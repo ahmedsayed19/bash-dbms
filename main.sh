aa60f8ff9efebd0421b2097fe98abed8f55ac355
Ahmed Sayed <ahmedsayed19>
2023-04-06 04:13:10 +0200

    main menu functionality without any validation

--- main.sh
+++ main.sh
@@ -3,7 +3,6 @@ database_menu(){
     database_options=( "Create_Table"  "List_Tables" "Drop_Table" "Insert_into_Table" "Select_From_Table" "Delete_From_Table" "Update_Table" "Quite")
     select option in ${database_options[@]}
     do
-        
         case $option in
         # Two case values are declared here for matching
         Create_Table)
@@ -28,6 +27,7 @@ database_menu(){
         echo "Update_Table"
         ;;
         Quite)
+        PS3="bash bdms #?>"
         echo "Quite"
         break
         ;;
@@ -38,26 +38,43 @@ database_menu(){
     done
 }
 
+if [[ -d bash_dbms ]]
+then
+    cd bash_dbms
+else
+    mkdir bash_dbms
+    cd bash_dbms
+fi
 
-home_options=("Create_Database" "List_Databases" "Connect_To_Databases" "Drop_Database")
-
+home_options=("Create_Database" "List_Databases" "Connect_To_Databases" "Drop_Database" "Exit")
+PS3="bash bdms #?>"
 select option in ${home_options[@]}
 do
     
     case $option in
     # Two case values are declared here for matching
     Create_Database)
-    echo "Create Database"
+        read -p "Enter the Database Name: " db
+        mkdir $db
+        # echo "Create Database"
     ;;
     List_Databases)
-    echo "List Database"
+        echo "Databases: "
+        ls
     ;;
     Connect_To_Databases)
-    echo "Connect To Database"
-    database_menu
+        ls
+        read -p "Enter the Database Name: " db
+        cd $db
+        PS3="$db #?>"
+        database_menu
     ;;
     Drop_Database)
-    echo "Drop Database"
+        read -p "Enter the Database Name: " db
+        rm -r $db
+    ;;
+    Exit)
+        break
     ;;
     *)
     echo "Invalid Input."
